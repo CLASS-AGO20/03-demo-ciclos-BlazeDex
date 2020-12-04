77c8187d33236e9b102b808f0ae9ef7a82fad9a8
Alan Hernández <ahernandez102@ucol.mx>
2020-12-04 14:59:33 -0600

    Implemented contarImparesFor()

--- src/main.js
+++ src/main.js
@@ -8,8 +8,26 @@ export default class App {
         }
         return suma;
     }
+
+    contarImparesFor(inicio, fin) {
+        let counter = 0;
+
+        if(inicio > fin) {
+            let c = inicio;
+            inicio = fin;
+            fin = c;
+        }
+
+        for(let i = inicio; i <= fin; i = i + 1) {
+            if(i % 2 !== 0) {
+                counter = counter + 1;
+            }
+        }
+        return counter;
+    }
 }
 
 let app = new App();
 
 console.log(app.sumarParesFor());
+console.log(app.contarImparesFor(1, 15));
